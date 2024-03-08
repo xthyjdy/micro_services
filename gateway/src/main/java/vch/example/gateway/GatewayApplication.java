92226838d48edb16c9a96b20b8896c3700447d15
vch <ftestf32@gmail.com>
2024-03-08 21:58:49 +0100

    spring micro_services base setup (case "discovery")

---
+++ gateway/src/main/java/vch/example/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package vch.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://www.youtube.com/watch?v=KJ0cSvYj41c&list=PPSV  16.00
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
